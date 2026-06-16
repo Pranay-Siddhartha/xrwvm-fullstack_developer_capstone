bc2f6b3b7931e0a247f4d527ef4d317b050a8f0f
Pranay-Siddhartha <pranay.tamada@gmail.com>
2026-06-16 10:37:30 -0400

    Adding temporary changes to Github

--- server/database/app.js
+++ server/database/app.js
@@ -49,7 +49,7 @@ app.get('/fetchReviews', async (req, res) => {
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
   try {
-    const documents = await Reviews.find({dealership: req.params.id});
+    const documents = await Reviews.find({dealership: parseIn(req.params.id)});
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -57,18 +57,48 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 });
 
 // Express route to fetch all dealerships
+// Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({
+      error: 'Error fetching documents'
+    });
+  }
 });
+
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  try {
+    const documents = await Dealerships.find({
+      st: req.params.state
+    });
+
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({
+      error: 'Error fetching documents'
+    });
+  }
 });
+
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try {
+    const document = await Dealerships.findOne({
+      id: parseInt(req.params.id)
+    });
+
+    res.json(document);
+  } catch (error) {
+    res.status(500).json({
+      error: 'Error fetching documents'
+    });
+  }
 });
 
 //Express route to insert review
