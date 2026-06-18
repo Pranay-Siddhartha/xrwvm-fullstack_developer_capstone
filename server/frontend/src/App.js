399c8af5531c75ddd4c106a859a735e29a6c122a
Pranay-Siddhartha <pranay.tamada@gmail.com>
2026-06-18 10:54:13 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
 import PostReview from "./components/Dealers/PostReview";
+import Dealer from "./components/Dealers/Dealer"
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/postreview/:id" element={<PostReview/>} />
         <Route path="/dealers" element={<Dealers/>} />
+        <Route path="/dealer/:id" element={<Dealer/>} />
+
 
 
     </Routes>
